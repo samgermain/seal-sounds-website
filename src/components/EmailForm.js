e6189cef8382871231fa89c3d617112cdaf211b6
samgermain <sam.germain@usask.ca>
2021-02-14 17:10:19 -0600

    Chaned style of email sending

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -3,31 +3,32 @@ import emailjs from 'emailjs-com';
 // import ReCaptcha, { Loader } from "@pittica/gatsby-plugin-recaptcha"
 // var Recaptcha = require('react-recaptcha');
 
-const FormControl = ({label, page}) => {
-    switch (label) {
-        case 'Name':
-            return <input className="form-control" type='text' name='from_name' placeholder={label} />
-            break;
-        case 'Email':
-            return <input className="form-control" type='email' name='from_email' placeholder={label} />
-            break;
-        case 'Subject':
-            return <input className="form-control" name='subject' type='text' placeholder={label} />
-            break;
-        case 'Message':
-            return <textarea className='form-control' name='html_message' placeholder={label} />
-            break;
-    };
-};
+export default ({className} ) => {
 
-export default ({className, page} ) => {
+    const formInput = {
+        "from_name": "",
+        "email_subject": "",
+        "from_email": "",
+        "html_message": ""
+    };
+
+    const FormInput = ({label, children}) => (
+        <div className='row text-right form-group' >
+            <label className="col-sm-2" >
+                {label}
+            </label>
+            <div className='col-sm-10'>
+                {children}
+            </div>
+        </div>
+    );
 
     // Loader()
     const sendEmail = function(e){
 //        const [serviceId, templateId, userId] = ['gmail', 'contactform', 'user_1vdH1saKOwWgYvrTaAnpV']
         e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
         console.log(e.target)
-        emailjs.send('gmail', 'emailform', e.target, 'user_1vdH1saKOwWgYvrTaAnpV').then((result) => {
+        emailjs.send('gmail', 'emailform', formInput, 'user_1vdH1saKOwWgYvrTaAnpV').then((result) => {
             window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
         }, (error) => {
             console.log(error.text);
@@ -36,16 +37,41 @@ export default ({className, page} ) => {
 
     return (
         <form method="post" id='emailForm' className={className} onSubmit={sendEmail}>
-            {['Name', 'Email', 'Subject', 'Message'].map(label => (
-                <div className='row text-right form-group' >
-                    <label className="col-sm-2" >
-                        {label}
-                    </label>
-                    <div className='col-sm-10'>
-                        <FormControl label={label} page={page} />
-                    </div>
-                </div>
-            ))}
+            <FormInput label="Name">
+                <input
+                    className="form-control"
+                    type='text'
+                    name='from_name'
+                    placeholder="Name" 
+                    onChange={e => formInput.from_name = e.target.value}
+                />
+            </FormInput>
+            <FormInput label="Email">
+                <input
+                    className="form-control"
+                    type='email'
+                    name='from_email'
+                    placeholder="Email"
+                    onChange={e => formInput.from_email = e.target.value}
+                />
+            </FormInput>
+            <FormInput label="Subject">
+                <input
+                    className="form-control" 
+                    name='email_subject' 
+                    type='text' 
+                    placeholder="Subject" 
+                    onChange={e => formInput.email_subject = e.target.value}
+                />
+            </FormInput>
+            <FormInput label="Message">
+                <textarea 
+                    className='form-control' 
+                    name='html_message' 
+                    placeholder="Message" 
+                    onChange={e => formInput.html_message = e.target.value}
+                />
+            </FormInput>
             <div className='row ml-auto col-sm-10'>
                 {/* <div className="m-auto m-sm-0 g-recaptcha" data-sitekey="6Le1Y1caAAAAAE-SskS-F9EHwsxdwi_MlJN5UF8q"></div> */}
                 <div className='form-group ml-auto mb-md-auto'>
