b2dec67a3309f1deb16e0f87ecdd89706605bcb9
samgermain <sam.germain@usask.ca>
2021-02-15 15:10:35 -0600

    Removed gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-preact`,
-    `gatsby-plugin-netlify`,
+    // `gatsby-plugin-netlify`,
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-manifest`,
